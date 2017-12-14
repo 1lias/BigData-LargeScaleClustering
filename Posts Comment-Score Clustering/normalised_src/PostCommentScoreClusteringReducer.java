cdacfb59fff95d41ce84ad1ac0eeeacedb8ffa52
Efthymios Chatziathanasiadis <ec305@itl046.student.eecs.qmul.ac.uk>
2017-12-14 22:17:33 +0000

    Normalised 2D Clustering

---
+++ Posts Comment-Score Clustering/normalised_src/PostCommentScoreClusteringReducer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.DoubleWritable;
+
+
+public class PostCommentScoreClusteringReducer extends Reducer<DoubleDoublePair, DoubleDoublePair, DoubleDoublePair, NullWritable> {
+
+    public void reduce(DoubleDoublePair centroid,
+                       Iterable<DoubleDoublePair> data, Context context)
+												throws IOException, InterruptedException {
+       double sumX = 0;
+       double sumY = 0;
+       int numEl = 0;
+
+       for(DoubleDoublePair commentCount_Score : data){
+         sumX += commentCount_Score.getX().get();
+         sumY += commentCount_Score.getY().get();
+         numEl++;
+       }
+
+       double newCentroidX = (sumX / numEl);
+       double newCentroidY = (sumY / numEl);
+
+       DoubleDoublePair newCentroid = new  DoubleDoublePair(newCentroidX,newCentroidY);
+
+       context.write(newCentroid, NullWritable.get());
+    }
+}
